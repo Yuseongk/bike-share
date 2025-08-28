70444d3872cca5ad0f1b8181fa51a74f0a3c2385
Yuseongk <jaykim20081226@gmail.com>
2025-08-28 15:49:35 +0200

    Update Main.java

--- bike share/src/Main.java
+++ bike share/src/Main.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        double tripMinutes = 138470.3;
-        int roundTripMinute = (int)(tripMinutes + 0.5);
+        System.out.println("trip minute?");
+        Scanner scan = new Scanner(System.in);
+        String tripMinutes = scan.nextLine();
+        int trip = Integer.parseInt(tripMinutes);
+        int roundTripMinute = (int)(trip + 0.5);
         double unlockFee = 10.0;
         double perMinute = 0.01;
-        double deviceFee = tripMinutes * perMinute;
+        double deviceFee = trip * perMinute;
         double tax = (deviceFee + unlockFee) * 0.21;
         double total = deviceFee + tax;
         System.out.println("minutes: " + roundTripMinute);
